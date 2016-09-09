a222ffcaf180fce1850e425e9a578703298578e8
robneu <rob@wpsitecare.com>
2016-09-09 15:14:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Find Shortcodes
+
+A small utility for searching WordPress posts for a given shortcode.
+
+__Contributors:__ [Robert Neu](https://github.com/robneu)  
+__Requires:__ WordPress 4.6  
+__Tested up to:__ WordPress 4.6  
+__License:__ [MIT](http://wpsitecare.mit-license.org/)  
---
+++ views/shortcodes.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Template part for displaying all theme and plugin slugs.
+ *
+ * @package   FindShortcodes\Admin\Views
+ * @copyright Copyright (c) 2016, WP Site Care, LLC
+ * @license   MIT
+ * @since     0.1.0
+ */
+
+$shortcode = empty( $data['sitecare_fsc_shortcode'] ) ? '' : $data['sitecare_fsc_shortcode'];
+?>
+<div id="sc-find-shortcodes" class="wrap sc-find-shortcodes">
+	<h1>Find Shortcodes</h1>
+
+	<form method="post" class="shortcode-search-form">
+		<table class="form-table">
+			<tbody>
+				<tr>
+					<th scope="row"><label for="sitecare_fsc_shortcode">Shortcode</label></th>
+					<td><input name="sitecare_fsc_shortcode" id="sitecare_fsc_shortcode" value="<?php echo esc_attr( $shortcode ); ?>" class="regular-text" type="text"></td>
+				</tr>
+			</tbody>
+		</table>
+
+		<p class="submit">
+			<button type="submit" name="sitecare_fsc_submit" id="submit" class="button button-primary" >
+				<?php esc_html_e( 'Search for Shortcode' ); ?>
+			</button>
+		</p>
+
+		<?php wp_nonce_field( 'find_shortcode', 'sitecare_fsc_submit_form' ); ?>
+	</form>
+
+	<?php if ( ! empty( $shortcode ) ) : ?>
+
+		<section id="found-shortcodes-content" class="found-shortcodes-content">
+
+			<?php if ( $posts = sitecare_fsc_get_posts_with_shortcode( esc_attr( $shortcode ) ) ) : ?>
+
+				<h2>Posts with the <?php echo esc_attr( $shortcode ); ?> Shortcode</h2>
+
+					<ul>
+						<?php foreach ( $posts as $post_id ) : ?>
+
+							<li>
+								<p>
+									<code><?php echo $post_id; ?></code> -
+									<a target="_blank" href="<?php echo get_permalink( $post_id ); ?>">
+										<?php get_the_title( $post_id ); ?>
+									</a>
+								</p>
+							</li>
+
+						<?php endforeach; ?>
+					</ul>
+
+			<?php else : ?>
+
+				<h2>No posts with the <?php echo esc_attr( $shortcode ); ?> Shortcode could be found.</h2>
+
+			<?php endif; ?>
+
+		</section><!-- #found-shortcodes-content -->
+
+	<?php endif; ?>
+
+</div><!-- #sc-find-shortcodes -->
